dfb48a1009b8d82a7bad3436f7613a8c43ab24a2
nataliizab <nataliia.zab@gmail.com>
2023-01-30 23:03:39 +0000

    Level 100

--- script.js
+++ script.js
@@ -1,12 +1,75 @@
 //You can edit ALL of the code here
-function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+// function setup() {
+//   const allEpisodes = getAllEpisodes();
+//   makePageForEpisodes(allEpisodes);
+// }
+
+// function makePageForEpisodes(episodeList) {
+//   const rootElem = document.getElementById("root");
+//   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+// }
+
+// window.onload = setup;
+
+let gameOfThronesEpisodes = getAllEpisodes();
+
+let rootElem = document.getElementById("root");
+
+function getOneEpisode(allEpisodes) {
+  for (let episode of allEpisodes) {
+    //main div for one episode
+    const mainDiv = document.createElement("div");
+    mainDiv.classList.add("episode");
+    rootElem.appendChild(mainDiv);
+
+    //title for the episode
+    const title = document.createElement("h1");
+    title.classList.add("episode-title");
+    title.textContent = episode.name;
+    mainDiv.appendChild(title);
+
+    //episode number
+    const episodeNumber = document.createElement("h2");
+    episodeNumber.textContent = `S${String(episode.season).padStart(
+      2,
+      "0"
+    )}E${String(episode.number).padStart(2, "0")}`;
+    episodeNumber.classList.add("episode-number");
+    mainDiv.appendChild(episodeNumber);
+
+    // container for episode image
+    const divForEpisodeImage = document.createElement("div");
+    divForEpisodeImage.classList.add("episode-image-container");
+    mainDiv.appendChild(divForEpisodeImage);
+
+    // episode image
+    const episodeImage = document.createElement("img");
+    episodeImage.src = episode.image.medium;
+    episodeImage.alt = episode.name;
+    episodeImage.classList.add("episode-image");
+    divForEpisodeImage.appendChild(episodeImage);
+
+    // episode summary
+    const episodeSummary = document.createElement("div");
+    episodeSummary.innerHTML = episode.summary;
+    mainDiv.appendChild(episodeSummary);
+    // mainDiv.appendChild(document.createElement("br"));
+  }
 }
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
+getOneEpisode(gameOfThronesEpisodes);
 
-window.onload = setup;
+const footer = document.createElement("footer");
+document.querySelector("body").appendChild(footer);
+const footerLink = document.createElement("a");
+footerLink.href = "https://www.tvmaze.com/";
+footerLink.target = "_blank";
+footerLink.textContent = "© TVmaze.com";
+footer.appendChild(footerLink);
+// get.appendChild(footer);
+
+//
+
+// Your page should state somewhere that the data has (originally) come from TVMaze.com,
+// and link back to that site (or the specific episode on that site). See tvmaze.com/api#licensing.
